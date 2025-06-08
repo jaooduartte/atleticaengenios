f2955477aed48736935220fb1a062a4ec6119438
Joao Duarte <jpdx2010@hotmail.com>
2025-06-08 17:06:15 -0300

    fix: Correçào para limitar a 5mb o envio de fotos no servidor

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,11 @@
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  }
+});
 const authenticate = require('./src/middleware/auth.middleware');
 const express = require('express');
 const cors = require('cors');
