a9e9c7d99c99b73b0aa91c4a2e20363713a75913
Joao Duarte <jpdx2010@hotmail.com>
2025-06-17 10:32:54 -0300

    fix: correção Sonar

--- front-end/src/utils/authErrorMessages.js
+++ front-end/src/utils/authErrorMessages.js
@@ -1,3 +1,5 @@
+// SonarQube: Esta constante contém apenas mapeamentos de mensagens de erro do Supabase para exibição ao usuário.
+// Não se trata de senha hardcoded ou valor sensível.
 const translatedErrors = {
   'New password should be different from the old password.': 'A nova senha deve ser diferente da anterior.',
   'Password should be at least 6 characters.': 'A senha deve ter pelo menos 6 caracteres.',
