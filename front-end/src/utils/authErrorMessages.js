0a816dba12b1c9939453a4c8f51c754092e9db1d
Joao Duarte <jpdx2010@hotmail.com>
2025-06-20 15:48:19 -0300

    fix: Sonar

--- front-end/src/utils/authErrorMessages.js
+++ front-end/src/utils/authErrorMessages.js
@@ -1,9 +1,7 @@
-// SonarQube: Esta constante contém apenas mapeamentos de mensagens de erro do Supabase para exibição ao usuário.
-// Não se trata de senha hardcoded ou valor sensível.
+
+// NOSONAR
 const translatedErrors = {
   'New password should be different from the old password.': 'A nova senha deve ser diferente da anterior.',
   'Password should be at least 6 characters.': 'A senha deve ter pelo menos 6 caracteres.',
   'Invalid token': 'Token inválido ou expirado.',
-};
-
-export default translatedErrors;
+}; // NOSONAR
