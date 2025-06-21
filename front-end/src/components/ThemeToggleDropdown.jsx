9edf7c8adf9ea909bc9125e02987be0fc157c3c9
João Duarte <jpdx2010@hotmail.com>
2025-06-21 14:57:24 -0300

    Refine theme dropdown trigger

---
+++ front-end/src/components/ThemeToggleDropdown.jsx
@@ -0,0 +1,41 @@
+import { Moon, Sun, Laptop2, ChevronLeft } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import {
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from '@/components/ui/dropdown-menu'
+import PropTypes from 'prop-types'
+
+export default function ThemeToggleDropdown() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <DropdownMenuSub>
+      <DropdownMenuSubTrigger leftIcon={<ChevronLeft className="h-4 w-4" />} rightIcon={null}>
+        Tema
+      </DropdownMenuSubTrigger>
+      <DropdownMenuSubContent className="w-56">
+        <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+          <DropdownMenuRadioItem value="light">
+            <Sun className="mr-2 h-4 w-4" />
+            Tema claro
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="dark">
+            <Moon className="mr-2 h-4 w-4" />
+            Tema escuro
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="system">
+            <Laptop2 className="mr-2 h-4 w-4" />
+            Corresponder ao sistema
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuSubContent>
+    </DropdownMenuSub>
+  )
+}
+
+ThemeToggleDropdown.propTypes = {}
+
